5950b7cbd1fdb5e718ad113049bf5cb435c24924
KasukabeDefenceForce <abhinavohri13@gmail.com>
2025-11-14 03:47:36 +0530

    Fix circular import error

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from app.routes import main, products, webhooks, upload
+
 
 main_bp = Blueprint('main', __name__)
 product_bp = Blueprint('products', __name__)
 webhook_bp = Blueprint('webhooks', __name__)
 
+from app.routes import main, products, webhooks, upload
