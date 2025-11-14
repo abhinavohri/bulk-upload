7c2e876ed3b38a29ee3f18df2593aa967b77b938
KasukabeDefenceForce <abhinavohri13@gmail.com>
2025-11-14 10:32:39 +0530

    Activate citext extension

--- init_db.py
+++ init_db.py
@@ -1,11 +1,16 @@
 from app import create_app, db
 import os
+from sqlalchemy.sql import text
 
 def init_database():
     """Initialize the database"""
     app = create_app(os.environ.get('FLASK_ENV'))
 
     with app.app_context():
+        db.session.execute(text("CREATE EXTENSION IF NOT EXISTS citext;"))
+        db.session.commit()
+
+        print("CITEXT extension enabled.")
         db.create_all()
         print("Database tables created successfully!")
 
